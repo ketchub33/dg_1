191d7421adeb927e6f3df80eb8fa291799db9105
ketchub33 <silvanih2007@gmail.com>
2026-03-31 22:38:21 +0500

    Initial commit: Clean structure

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&0t=)_6!y&w3gnaj5q8e9hd11i&qwx6ugx)3h2ek_d-d21k+n)'
+SECRET_KEY = 'django-insecure-cm!wozu%2b%q+#iz62@ou(wn=y5-ie!z=b#!z!u#4_ra9eg*8q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
